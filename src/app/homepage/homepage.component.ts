88e219410e41e017e0926c35fe25dc53efca30ef
Berke Filiz <edu.berkefiliz@gmail.com>
2022-02-20 17:23:56 +0100

    Add homepage

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['../app.component.scss']
+})
+export class HomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
